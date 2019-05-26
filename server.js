19940fdaa798940271dd1dc8649a0822e7acd8b4
Max <dragoonz@yandex.ru>
2019-05-26 03:49:39 +0300

    fesf

--- server.js
+++ server.js
@@ -6,10 +6,10 @@ var app = express()
 var port = 8080
 var httpServer = http.createServer(app)
 
-app.use(express.static(__dirname + '/client/build'))
+app.use(express.static(__dirname + '/gatchina-hackathon/build'))
 
-app.get('/client', function(req, res) {
-  res.sendFile(__dirname + '/client/build/index.html')
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/gatchina-hackathon/build/index.html')
 })
 
 httpServer.listen(port, () => console.log('server is listening'))
